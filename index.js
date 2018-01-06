2325cb88c2a79dac9373d831880f590dcedee105
Eirik S. Morland <eirik@morland.no>
2018-01-06 19:59:50 +0100

    Update some packages

--- index.js
+++ index.js
@@ -1,102 +1,100 @@
-module.exports = EoNotify;
+module.exports = notifier
 
-var Eo = require('e-o');
-var nodemailer = require('nodemailer');
-var wellknown = require('nodemailer-wellknown');
-var htmlToText = require('nodemailer-html-to-text').htmlToText;
-var path = require('path');
-var templatesDir = path.join(__dirname, 'templates');
-var emailTemplates = require('email-templates');
-var util = require('util');
-var fs = require('fs');
+const nodemailer = require('nodemailer')
+var htmlToText = require('nodemailer-html-to-text').htmlToText
+var path = require('path')
+var templatesDir = path.join(__dirname, 'templates')
+const Email = require('email-templates')
+var util = require('util')
 
-var config = require.main.require('./notify-config');
+var config = require.main.require('./notify-config')
 if (!config) {
-  throw new Exception('No config found. Please configure this module.');
+  throw new Error('No config found. Please configure this module.')
 }
-var transporter = nodemailer.createTransport({
+let transporter = nodemailer.createTransport({
   service: 'sparkpost',
   auth: {
     user: 'SMTP_Injection',
     pass: config.sparkpostPass
   }
-});
-transporter.use('compile', htmlToText());
+})
+transporter.use('compile', htmlToText())
 
-function debuglines(data) {
-  return util.format("%d [%s]: %s", parseInt(data.timestamp, 10), data.type, data.message);
+function debuglines (data) {
+  return util.format('%d [%s]: %s', parseInt(data.timestamp, 10), data.type, data.message)
 }
 
-function makeSubject(type, d) {
-  var subject = '';
+function makeSubject (type, d) {
+  var subject = ''
   switch (type) {
     case 'notify-down':
-      subject = util.format('Response code %d from %s', Number(d.code), d.site);
-      break;
+      subject = util.format('Response code %d from %s', Number(d.code), d.site)
+      break
 
     case 'notify-process':
-      subject = util.format('Timeout on %s', d.site);
-      break;
+      subject = util.format('Timeout on %s', d.site)
+      break
 
     case 'notify-resource':
-      subject = util.format('Resource error on %s', d.site);
-      break;
+      subject = util.format('Resource error on %s', d.site)
+      break
   }
   if (d.fixed) {
-    subject = 'FIXED: ' + subject;
+    subject = 'FIXED: ' + subject
   }
-  return subject;
+  return subject
 }
 
-function EoNotify(type, data, opts, callback) {
-  emailTemplates(templatesDir, function(err, template) {
-    // Assemble some data.
-    var d = {
-      site: opts.url,
-      code: data.statusCode,
-      debugData: data.logs.map(debuglines).join("\n"),
-      numbers: data.numbers,
-      fixed: data.fixed
-    };
-    d.subject = makeSubject(type, d);
-    template(type, d, function(err, h, t) {
-      if (err) {
-        console.log(err);
-        return;
+function notifier (type, data, opts, callback) {
+  let email = new Email({
+    views: {
+      root: path.join(__dirname, 'templates'),
+      options: {
+        extension: 'ejs'
       }
-      var attach = false;
-      if (data.screenshot) {
-        attach = true;
-      }
-      var sendData = {
-        from: util.format('%s <%s>', config.fromUser, config.fromMail),
-        to: opts.email,
-        subject: d.subject,
-        html: h
-      };
-      if (attach) {
-        sendData.attachments = [
-          {
-            path: data.screenshot.trim()
-          }
-        ];
-      }
-      var _callback = function(err, res) {
-        if (err) console.error(err);
-        if (callback) {
-          callback(err, res);
+    },
+    transport: transporter
+  })
+  // Assemble some data.
+  var d = {
+    site: opts.url,
+    code: data.statusCode,
+    debugData: data.logs.map(debuglines).join('\n'),
+    numbers: data.numbers,
+    fixed: data.fixed
+  }
+  d.subject = makeSubject(type, d)
+  email.render(type, d).then(function (h) {
+    var attach = false
+    if (data.screenshot) {
+      attach = true
+    }
+    var sendData = {
+      from: util.format('%s <%s>', config.fromUser, config.fromMail),
+      to: opts.email,
+      subject: d.subject,
+      html: h
+    }
+    if (attach) {
+      sendData.attachments = [
+        {
+          path: data.screenshot.trim()
         }
-        else {
-          if (err) {
-            console.error(err);
-          }
+      ]
+    }
+    var _callback = function (err, res) {
+      if (err) console.error(err)
+      if (callback) {
+        callback(err, res)
+      } else {
+        if (err) {
+          console.error(err)
         }
-      };
-      transporter.sendMail(sendData, _callback);
-    });
-  });
+      }
+    }
+    transporter.sendMail(sendData, _callback)
+  })
+  .catch(function (e) {
+    callback(e)
+  })
 }
-
-EoNotify.prototype.log = function() {
-  console.log(arguments);
-};
